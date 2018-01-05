6f3cb2ff60659dcb3012f28046e1e91c19cee6a9
Durev <vincent.durewski@gmail.com>
2018-01-05 15:24:43 +0100

    Add Images model and controller with views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'images#upload'
+  get '/display', to: 'images#display'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
