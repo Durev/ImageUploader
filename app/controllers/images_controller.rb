3958bc2c3812518ecc309b764ccbc796ded499b2
Durev <vincent.durewski@gmail.com>
2018-01-05 16:59:54 +0100

    Add CarrierWave gem and an image creation form without uploading

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,26 @@
 class ImagesController < ApplicationController
   def upload
+    @image = Image.new
   end
 
   def display
+    @images = Image.all
   end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to display_path
+      # add flash success
+    else
+      render 'root'
+      # flash error
+    end
+  end
+
+  private
+
+    def image_params
+      params.require(:image).permit(:name, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'images#upload'
   get '/display', to: 'images#display'
+  resources :images
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
